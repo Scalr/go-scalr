cb4186ab3cd3ce169b049eca896f5ad868ad722d
Vitalii Motso <vitalii.motso@gmail.com>
2023-12-16 14:19:30 +0200

    Add ConfigurationVesrion relation to the Workspace

--- workspace.go
+++ workspace.go
@@ -103,13 +103,14 @@ type Workspace struct {
 	VarFiles                  []string               `jsonapi:"attr,var-files"`
 
 	// Relations
-	CurrentRun    *Run           `jsonapi:"relation,current-run"`
-	Environment   *Environment   `jsonapi:"relation,environment"`
-	CreatedBy     *User          `jsonapi:"relation,created-by"`
-	VcsProvider   *VcsProvider   `jsonapi:"relation,vcs-provider"`
-	AgentPool     *AgentPool     `jsonapi:"relation,agent-pool"`
-	ModuleVersion *ModuleVersion `jsonapi:"relation,module-version,omitempty"`
-	Tags          []*Tag         `jsonapi:"relation,tags"`
+	CurrentRun           *Run                  `jsonapi:"relation,current-run"`
+	Environment          *Environment          `jsonapi:"relation,environment"`
+	CreatedBy            *User                 `jsonapi:"relation,created-by"`
+	VcsProvider          *VcsProvider          `jsonapi:"relation,vcs-provider"`
+	AgentPool            *AgentPool            `jsonapi:"relation,agent-pool"`
+	ModuleVersion        *ModuleVersion        `jsonapi:"relation,module-version,omitempty"`
+	Tags                 []*Tag                `jsonapi:"relation,tags"`
+	ConfigurationVesrion *ConfigurationVersion `jsonapi:"relation,configuration-version"`
 }
 
 // Hooks contains the custom hooks field.
