6899ad8dd242cf0e5702e5dae8ce9754c075b64f
penja <a.penchuk@scalr.com>
2026-03-20 11:11:03 +0200

    SCALRCORE-37753 API > disable urls in resources with internal, system endpoints

--- agent_pool_test.go
+++ agent_pool_test.go
@@ -80,7 +80,7 @@ func TestAgentPoolsCreate(t *testing.T) {
 			Name:           String("test-provider-pool-" + randomString(t)),
 			VcsEnabled:     Bool(true),
 			WebhookEnabled: Bool(true),
-			WebhookUrl:     String("http://example.com"),
+			WebhookUrl:     String("https://example.com"),
 			WebhookHeaders: []*AgentPoolHeader{
 				{
 					Name:      "Authorization",
